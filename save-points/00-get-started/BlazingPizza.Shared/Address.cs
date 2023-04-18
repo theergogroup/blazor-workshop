786596a3217b4cbbe4afde3f1ee558cb15ed96bf
Cinthia Almaraz Sierra <cinthia.almaraz@theergogroup.com>
2023-04-18 12:42:46 -0600

    Session 4 and 5

--- save-points/00-get-started/BlazingPizza.Shared/Address.cs
+++ save-points/00-get-started/BlazingPizza.Shared/Address.cs
@@ -1,18 +1,27 @@
-﻿namespace BlazingPizza;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class Address
+namespace BlazingPizza
 {
-    public int Id { get; set; }
+    public class Address
+    {
+        public int Id { get; set; }
 
-    public string Name { get; set; }
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
 
-    public string Line1 { get; set; }
+        [Required, MaxLength(100)]
+        public string Line1 { get; set; }
 
-    public string Line2 { get; set; }
+        [MaxLength(100)]
+        public string Line2 { get; set; }
 
-    public string City { get; set; }
+        [Required(ErrorMessage = "How do you expect to receive the pizza if we don't even know what city you're in?"), MaxLength(50)]
+        public string City { get; set; }
 
-    public string Region { get; set; }
+        [Required, MaxLength(20)]
+        public string Region { get; set; }
 
-    public string PostalCode { get; set; }
+        [Required, MaxLength(20)]
+        public string PostalCode { get; set; }
+    }
 }
